44288fe8a8775c3d01b5eff235872eb218471df2
Daniymb <deyumbo@uce.edu.ec>
2024-06-18 11:31:25 -0500

    Add files via upload

---
+++ Index/Index.js
@@ -0,0 +1,15 @@
+function onClickSubmitButton(){
+
+    let inputValue = document.getElementById("imputText").value;
+     if (inputValue > 0){
+        alert ("Hey el numero es mayor que cero ");
+    }
+
+    if (inputValue < 0){
+        alert ("Hey el numero es menor que cero ");
+    }
+    
+    if (inputValue = 0){
+        alert ("Intente otra vez");
+    }
+}
